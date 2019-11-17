a1d181aebd6e2fa5d36adbdfeb310be728737931
Velor <velorkhalilov@yandex.ru>
2019-11-17 17:26:58 +0300

    Добавил части приложения

---
+++ Model/History.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OneNote.Model
+{
+    class History : Base
+    {
+        public string Table { get; set; }
+        public string Field { get; set; }
+        public string PrevValue { get; set; }
+        public string NewValue { get; set; }
+        public int Autor { get; set; }
+    }
+}
