0fd26e7325dcab9cc812d716f031d18f0387eb7d
Velor <velorkhalilov@yandex.ru>
2019-11-17 18:27:53 +0300

    Добавил интерфейс базы данных

---
+++ SQLite/Model/HistoryModel.cs
@@ -0,0 +1,13 @@
+﻿using OneNote.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OneNote.SQLite.Model
+{
+    public class HistoryModel
+    {
+        public IEnumerable<HistoryRecord> Records { get; set; }
+        public IEnumerable<HistoryDetail> Details { get; set; }
+    }
+}
