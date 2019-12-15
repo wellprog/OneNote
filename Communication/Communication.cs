a562e6bfc661abde3ad0e8a9a13dfedf9f9023d7
Wellprog <velorkhalilov#gmail.com>
2019-12-15 16:08:34 +0300

    Добавил коменты

--- Communication/Communication.cs
+++ Communication/Communication.cs
@@ -8,16 +8,39 @@ namespace OneNote.Communication
 {
     class Communication : ICommunication
     {
+        /// <summary>
+        /// Проверить логин пароль (на непустые)
+        /// сделать запрос
+        /// Если ок, добавить в локальную БД
+        /// </summary>
+        /// <param name="login"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
         public string Authorize(string login, string password)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Отправить запрос
+        /// Залить все в базу
+        /// 
+        /// или если в базе уже есть тогда запросить историю
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
         public BookModel GetBooks(string token)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Получить историю и записать в базу
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="table"></param>
+        /// <param name="lastID"></param>
+        /// <returns></returns>
         public HistoryModel GetHistory(string token, string table, int lastID)
         {
             throw new NotImplementedException();
