4ab866741c81932e11f86b037ffe97eb4bcf1fd2
Farkhad <farx2222@gmail.com>
2020-02-02 18:59:21 +0300

    Add SectionViewModel

--- Application/ViewModel/BooksViewModel.cs
+++ Application/ViewModel/BooksViewModel.cs
@@ -1,4 +1,7 @@
-﻿using OneNote.Model;
+﻿using OneNote.Communication;
+using OneNote.Communication.Helpers;
+using OneNote.Helpers;
+using OneNote.Model;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,9 +11,10 @@ using System.Threading.Tasks;
 
 namespace OneNote.Application.ViewModel
 {
-    class BooksViewModel : INotifyPropertyChanged
+    public class BooksViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
 
         private List<Book> _books = new List<Book>();
         public List<Book> Books
@@ -28,13 +32,38 @@ namespace OneNote.Application.ViewModel
 
         public ClickCommand AddCommand { get; }
 
+        private ICommunication _communication;
+        private IEnviroment _enviroment;
+        private Client _client;
+
+        private string token = "";
+        private User currentUser;
+
+
         public BooksViewModel()
         {
             AddCommand = new ClickCommand(OnAddCommand);
+            _communication = ClassLoader.Instance.GetElement<ICommunication>();
+            _enviroment = ClassLoader.Instance.GetElement<IEnviroment>();
+            _client = ClassLoader.Instance.GetElement<Client>();
+
+            token = _enviroment.UserToken;
+            currentUser = _communication.GetUserDetails(token);
+
+            Books.AddRange(_client.GetLocalBooks(currentUser.ID));
         }
 
         protected void OnAddCommand(object param)
         {
+           
+            if(param is String)
+            {
+                Book book =_client.GetLocalBooks(currentUser.ID).Where(f => f.Name == param.ToString()).FirstOrDefault();
+                if (book == null)
+                {
+                    _client.AddLocalBook(new Book() { Name = param.ToString(), Autor = currentUser.ID });
+                }
+            }
 
         }
 
---
+++ Application/ViewModel/SectionViewModel.cs
@@ -0,0 +1,54 @@
+﻿using OneNote.Communication;
+using OneNote.Communication.Helpers;
+using OneNote.Helpers;
+using OneNote.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneNote.Application.ViewModel
+{
+   public class SectionViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ICommunication _communication;
+        private IEnviroment _enviroment;
+        private Client _client;
+
+        private string token = "";
+        private User currentUser;
+
+        private List<Section> _sections = new List<Section>();
+        public List<Section> Sections
+        {
+            get
+            {
+                return _sections;
+            }
+            set
+            {
+                _sections = value;
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(Section)));
+            }
+        }
+        public SectionViewModel()
+        {
+            _communication = ClassLoader.Instance.GetElement<ICommunication>();
+            _enviroment = ClassLoader.Instance.GetElement<IEnviroment>();
+            _client = ClassLoader.Instance.GetElement<Client>();
+
+            token = _enviroment.UserToken;
+            currentUser = _communication.GetUserDetails(token);
+        }
+
+        public IEnumerable<Section> FormBook(string book)
+        {
+           return _client.GetLocalSections(book);
+        }
+
+    }
+}
