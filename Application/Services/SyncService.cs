0657f02f58bed250d7670592b2597e4d390cd112
Wellprog <velorkhalilov#gmail.com>
2020-02-02 17:46:20 +0300

    Fix sync service

--- Application/Services/SyncService.cs
+++ Application/Services/SyncService.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OneNote.Application.Services
@@ -24,15 +25,27 @@ namespace OneNote.Application.Services
         }
 
         public void Start() {
-            //TODO start tmer
+            Thread th = new Thread(threadStart);
+            th.Start();
         }
         public void Stop() {
             //TODO stop timer
         }
 
+
+        void threadStart()
+        {
+            SyncBooks();
+            SyncSections();
+            SyncPages();
+
+            Thread.Sleep(60000);
+        }
+
         private void SyncBooks()
         {
             var data = _database.GetBooks(_enviroment.CurrentUser.ID);
+            //Если у нас ничего нет то нам нужно доставть все книги
             if (data.Count() == 0)
             {
                 var element = _communication.GetBooks(_enviroment.UserToken);
@@ -41,7 +54,8 @@ namespace OneNote.Application.Services
                     _database.AddBook(book);
 
                 _enviroment.RemoteBookCursor = element.LastHistory;
-            } else
+            }
+            else
             {
                 //Отправить
                 var toSend = _database.GetBookHistory(_enviroment.LocalBookCursor);
@@ -54,6 +68,66 @@ namespace OneNote.Application.Services
                 {
                     _database.UpdateBookByHistory(element.Records, element.Details);
                     _enviroment.RemoteBookCursor = element.Records.Last().ID;
+                }
+            }
+        }
+
+        private void SyncSections()
+        {
+            var data = _database.GetSections(_enviroment.CurrentUser.ID);
+            //Если у нас ничего нет то нам нужно доставть все книги
+            if (data.Count() == 0)
+            {
+                var element = _communication.GetSections(_enviroment.UserToken);
+
+                foreach (var section in element.Sections)
+                    _database.AddSection(section);
+
+                _enviroment.RemoteSectionCursor = element.LastHistory;
+            }
+            else
+            {
+                //Отправить
+                var toSend = _database.GetSectionHistory(_enviroment.LocalSectionCursor);
+                _communication.SetSectionHistory(_enviroment.UserToken, toSend);
+
+                //Получаем
+                var element = _communication.GetHistory(_enviroment.UserToken, "Section", _enviroment.RemoteSectionCursor);
+
+                if (element.Records.Count() > 0)
+                {
+                    _database.UpdateSectionByHistory(element.Records, element.Details);
+                    _enviroment.RemoteSectionCursor = element.Records.Last().ID;
+                }
+            }
+        }
+
+        private void SyncPages()
+        {
+            var data = _database.GetPages(_enviroment.CurrentUser.ID);
+            //Если у нас ничего нет то нам нужно доставть все книги
+            if (data.Count() == 0)
+            {
+                var element = _communication.GetPages(_enviroment.UserToken);
+
+                foreach (var page in element.Pages)
+                    _database.AddPage(page);
+
+                _enviroment.RemotePageCursor = element.LastHistory;
+            }
+            else
+            {
+                //Отправить
+                var toSend = _database.GetPageHistory(_enviroment.LocalPageCursor);
+                _communication.SetPageHistory(_enviroment.UserToken, toSend);
+
+                //Получаем
+                var element = _communication.GetHistory(_enviroment.UserToken, "Page", _enviroment.RemotePageCursor);
+
+                if (element.Records.Count() > 0)
+                {
+                    _database.UpdatePageByHistory(element.Records, element.Details);
+                    _enviroment.RemotePageCursor = element.Records.Last().ID;
                 }
             }
         }
